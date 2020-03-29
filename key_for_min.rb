589dd7f921ed1b90d1655653e84a76bf916c48ee
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-29 16:50:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_value = Float::INFINITY
 
   name_hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
+    if value < lowest_value
       lowest_value = value
       lowest_key = key
     end
